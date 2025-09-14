4fa05cc76fa3eb36c17ec610b56472ad46042998
Emmanuel <emmanuel.peter.oe@gmail.com>
2025-09-14 10:19:19 +0100

    Basic Arithmetic Exercise

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# Define variables
+principal = 1000     # Initial investment
+rate = 0.05          # Annual interest rate (5%)
+time = 3             # Time in years
+
+# Calculate simple interest
+interest = principal * rate * time
+
+# Print result
+print("The simple interest is:", interest)
