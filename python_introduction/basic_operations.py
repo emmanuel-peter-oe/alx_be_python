5ab8f04652ef36e74aa1d08062791926c0f33abc
Emmanuel <emmanuel.peter.oe@gmail.com>
2025-09-14 09:27:09 +0100

    Basic Arithmetic Exercise

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -4,3 +4,7 @@ number2 = 5
 print("Addition of", number1, "and", number2, "is", number1 + number2)
 print("Subtraction of", number1, "and", number2, "is", number1 - number2)
 print("Multiplication of", number1, "and", number2, "is", number1 * number2)
+
+Addition of 10 and 5 is 15
+Subtraction of 10 and 5 is 5
+Multiplication of 10 and 5 is 50
