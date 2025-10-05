346541bbcf876a24361771a96e48a054c9e620a9
Emmanuel <emmanuel.peter.oe@gmail.com>
2025-10-05 13:31:29 +0100

    Add BankAccount class and CLI interface (main-0.py)

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -18,5 +18,4 @@ class BankAccount:
         return False
 
     def display_balance(self):
-        """Print the current balance."""
-        print(f"Current Balance: ${self.__account_balance}")
+         print(f"Current Balance: ${self.account_balance:.2f}")
